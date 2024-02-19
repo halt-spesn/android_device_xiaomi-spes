76b2aa58b93bd4282070df8ae9d87431bde108fd
Guidix <81136142+GuidixX@users.noreply.github.com>
2024-02-19 13:25:52 +0200

    vendorsetup: Sync with GuidixX trees

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,6 @@
 echo "Syncing additional trees"
 rm -rf hardware/google/pixel/kernel_headers/Android.bp
 git clone https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi -b lineage-18.1
-git clone https://github.com/shifty-d/android_vendor_xiaomi_spes.git vendor/xiaomi/spes
-git clone https://github.com/muralivijay/kernel_xiaomi_sm6225 kernel/xiaomi/spes --depth=1
+git clone https://github.com/GuidixX/android_vendor_xiaomi_spes.git vendor/xiaomi/spes
+git clone https://github.com/GuidixX/kernel_xiaomi_spes kernel/xiaomi/spes --depth=1
 sed -i '18s/error/warning/' build/core/Makefile
