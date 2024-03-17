6017ef973adb6fab682d58c48a816e78ceb79ca5
shifty-d <ffoxy2870@gmail.com>
2024-03-17 19:10:07 +0200

    hotfix

--- device.mk
+++ device.mk
@@ -446,8 +446,6 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.2 \
-    android.hardware.power@1.2.vendor \
     android.hardware.power-service.xiaomi-libperfmgr \
     android.hardware.power.stats@1.0-service.mock
 
